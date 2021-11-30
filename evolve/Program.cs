7b20a0ef2ffce63b3f1878ed19d361c6f97e1066
Adam Johnson <columbus@adamlj.com>
2021-11-29 20:06:37 -0800

    Formatting in the Program.cs file

--- evolve/Program.cs
+++ evolve/Program.cs
@@ -1,21 +1,14 @@
 ﻿
 using System.Data.SqlClient;
 
-try
+var cnx = new SqlConnection(args[0]);
+var evolve = new Evolve.Evolve(cnx, msg => Console.WriteLine(msg))
 {
-    var cnx = new SqlConnection(args[0]);
-    var evolve = new Evolve.Evolve(cnx, msg => Console.WriteLine(msg))
-    {
-        Locations = new[] { args[1] },
-        IsEraseDisabled = true,
-        MetadataTableSchema = "migrations"
-    };
+    Locations = new[] { args[1] },
+    IsEraseDisabled = true,
+    MetadataTableSchema = "migrations"
+};
 
-    evolve.Migrate();
-}
-catch (Exception ex)
-{
-    throw;
-}
+evolve.Migrate();
 
 Console.WriteLine("Done Migrating DB");
